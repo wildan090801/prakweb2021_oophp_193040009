1a3e9e92e5d9d1f8cac1e857250b057c9959bbe5
nadliw090801 <muhammadwildan334@gmail.com>
2021-09-23 20:41:43 +0700

    Mempelajari Constanta

---
+++ oophp/constant.php
@@ -0,0 +1,31 @@
+<?php
+
+// define('NAMA', 'Muhammad Wildhan');
+// echo NAMA;
+
+// echo "<br>";
+
+// const UMUR = 20;
+// echo UMUR;
+
+// class Coba {
+//     const NAMA = 'Muhammad Wildhan';
+// }
+
+// echo Coba::NAMA;
+
+// echo __LINE__;
+
+// function coba() {
+//     return __FUNCTION__;
+// }
+
+// echo coba();
+
+class Coba {
+    public $kelas = __CLASS__;
+}
+
+$obj = new Coba;
+echo $obj->kelas;
+?>
